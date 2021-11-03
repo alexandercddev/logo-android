d4ddd8fe657b8309269cfb6b1fe38fef0ff9b096
Alexander Chi <arturochi2@hotmail.com>
2021-11-02 18:08:09 -0600

    Added animation style robot

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -1,6 +1,50 @@
 /** 
  *  @author alexandercddev
- *  @description Style Foodies Landing SASS
+ *  @description  
  *  @date 02/noviembre/2021 
 **/
 
+const defaultClass = '';
+
+const selectElement = document.getElementById('action');
+selectElement.addEventListener('change', ({ target}) => {
+    let head = document.getElementById('head');
+    let antennas = document.getElementById('antennas');
+    let body = document.getElementById('body');
+    let lengs = document.getElementById('lengs');
+    let container = document.getElementById('content-android');
+    head.className = ``;
+    antennas.className = ``;
+    body.className = ``;
+    lengs.className = ``;
+    container.className = ``;
+    switch(target.value) {
+        case 'hello': 
+        head.className = `animation-${target.value}`;
+            antennas.className = `animation-${target.value}`;
+            body.className = `animation-${target.value}`;
+            break;
+        case 'yes': 
+        case 'no':  
+            head.className = `animation-${target.value}`;
+            antennas.className = `animation-${target.value}`;
+            break;
+        case 'jump':
+            head.className = `animation-${target.value}`;
+            antennas.className = `animation-${target.value}`;
+            body.className = `animation-${target.value}`;
+            lengs.className = `animation-${target.value}`;
+            break;
+        case 'disarm':
+            head.className = `animation-${target.value}`;
+            antennas.className = `animation-${target.value}`;
+            body.className = `animation-${target.value}`;
+            lengs.className = `animation-${target.value}`;
+            break;
+        case 'cha':
+            body.className = `animation-${target.value}`;
+            lengs.className = `animation-${target.value}`;
+            container.className = `animation-${target.value}`;
+            break;
+    }
+}); 
